f951b8d800aebe3caf7d4d1f422016e07acbe62e
Ezequiel Villafañe <eze.villafae07@gmail.com>
2022-05-19 19:47:43 -0300

    fix test

--- src/test/ar/edu/ungs/census/blocks/domain/BlockRequestMother.java
+++ src/test/ar/edu/ungs/census/blocks/domain/BlockRequestMother.java
@@ -1,10 +1,11 @@
 package ar.edu.ungs.census.blocks.domain;
 
 import ar.edu.ungs.census.blocks.application.BlockRequest;
+import ar.edu.ungs.census.blocks.application.CoordinatesRequest;
 
 public class BlockRequestMother {
 
     public static BlockRequest random(){
-        return new BlockRequest(10L, CoordinatesMother.random());
+        return new BlockRequest(10L, CoordinatesRequestMother.random());
     }
 }
