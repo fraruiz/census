43b57ebe3f3f8c0e97294988f96d692b5ca4738e
NS cod <nicolas.sassoni@hotmail.es>
2022-05-21 18:12:23 -0300

    ratio saver and test

--- src/main/ar/edu/ungs/census/ratios/domain/RatioSaver.java
+++ src/main/ar/edu/ungs/census/ratios/domain/RatioSaver.java
@@ -1,3 +1,13 @@
 package ar.edu.ungs.census.ratios.domain;
 
-public final class RatioSaver {}
+public final class RatioSaver {
+    private final RatioRepository repository;
+
+    public RatioSaver(RatioRepository repository){
+        this.repository=repository;
+    }
+    public void save(Ratio ratio){
+
+        this.repository.save(ratio);
+    }
+}
