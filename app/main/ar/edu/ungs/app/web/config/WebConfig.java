d2eeacac4e644a6e847f02500171c3307e6f7282
Francisco Ruiz <franciscoruizlezcano@gmail.com>
2022-06-03 20:40:47 -0300

    fix: added imports

--- app/main/ar/edu/ungs/app/web/config/WebConfig.java
+++ app/main/ar/edu/ungs/app/web/config/WebConfig.java
@@ -1,9 +1,12 @@
 package ar.edu.ungs.app.web.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 @Configuration
 @EnableWebMvc
